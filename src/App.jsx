eac3c66ddbdec0514d5082308fa13fe3844e7a9c
bartosz121 <bmagiera121@gmail.com>
2022-05-24 14:19:09 +0200

    Remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
-import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
-import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <div className="App bg-base-100">
+    <div className="bg-base-100">
       <Navbar />
       <main>
         <Outlet />
