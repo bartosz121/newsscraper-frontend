68015e283cbf47a000bcee06f8664ad6a275c9a4
bartosz121 <bmagiera121@gmail.com>
2022-05-31 23:27:28 +0200

    Add bookmark feature
    - Logged in user can click ribbon in the upper right side corner to bookmark an article

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import useAuth from "../../hooks/useAuth/useAuth";
+import axiosI from "../../api/axios";
 
 import defaultNewsImg from "../../assets/defaultNewsImg.jpeg";
+import BookmarkIcon from "../BookmarkIcon/BookmarkIcon";
 
 const Article = (props) => {
   const {
@@ -11,6 +15,33 @@ const Article = (props) => {
     img_url: imgUrl,
     created,
   } = props.data;
+
+  const BOOKMARK_BASE_URL = import.meta.env.VITE_ST_APIDOMAIN;
+  const { isLoggedIn } = useAuth();
+  const [isMouseOver, setIsMouseOver] = useState(false);
+  const [isBookmarked, setIsBookmarked] = useState(null);
+
+  const addBookmark = async () => {
+    await axiosI
+      .post(`${BOOKMARK_BASE_URL}/bookmark`, { article_id: id })
+      .then((response) => setIsBookmarked(true))
+      .catch((error) => console.error(error));
+  };
+
+  const removeBookmark = async () => {
+    await axiosI
+      .delete(`${BOOKMARK_BASE_URL}/bookmark`, { data: { article_id: id } })
+      .then((response) => setIsBookmarked(false))
+      .catch((error) => console.error(error));
+  };
+
+  const handleBookmarkBtnClick = async () => {
+    if (isBookmarked) {
+      await removeBookmark();
+    } else {
+      await addBookmark();
+    }
+  };
 
   const validateImageUrl = (url) => {
     // some of the img urls have `width` parameter specified in them, replace it with some value here (TODO fix in api to not deal with it here)
@@ -23,10 +54,29 @@ const Article = (props) => {
   const getDate = (datetime) => {
     return datetime.split("T")[0];
   };
+
   return (
     <div
-      onClick={() => console.log(props.data)}
-      className="card w-full md:w-96 bg-base-300 bg-opacity-20 shadow-xl"
+      onClick={() => {
+        console.log(props.data);
+        console.log(`isBookmarked: ${isBookmarked}`);
+      }}
+      onMouseEnter={(e) => {
+        setIsMouseOver(true);
+        if (isBookmarked === null) {
+          axiosI
+            .post(`${BOOKMARK_BASE_URL}/bookmark/check`, {
+              article_id: id,
+            })
+            .then((response) => {
+              const is_bookmarked = response.data.is_bookmarked;
+              setIsBookmarked(is_bookmarked);
+            })
+            .catch((error) => console.error(error));
+        }
+      }}
+      onMouseLeave={(e) => setIsMouseOver(false)}
+      className="card w-full md:w-96 bg-base-100 shadow-xl"
     >
       <div className="card-body px-4 sm:px-8">
         <div className="flex flex-row items-center">
@@ -38,6 +88,11 @@ const Article = (props) => {
             />
           </div>
           <p className="ml-4 break-words">{title}</p>
+          {isLoggedIn && isMouseOver && (
+            <span onClick={(e) => handleBookmarkBtnClick()}>
+              <BookmarkIcon isBookmarked={isBookmarked} />
+            </span>
+          )}
         </div>
         <div className="mt-4 py-1 card-actions justify-center">
           <div
