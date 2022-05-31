cea70b00710d6ffb622927ab52b0f83a019650df
bartosz121 <bmagiera121@gmail.com>
2022-05-31 19:18:40 +0200

    Add new routes, add UserProvider, use env variables

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,78 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import SuperTokens, {
-  getSuperTokensRoutesForReactRouterDom,
-} from "supertokens-auth-react";
+import SuperTokens from "supertokens-auth-react";
 import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
 import Session from "supertokens-auth-react/recipe/session";
 
-// For supertokens auth component
-import * as reactRouterDom from "react-router-dom";
-
+import { UserProvider } from "./context/UserContext";
 import axiosI from "./api/axios";
+
 import Home from "./components/Home/Home";
+import Search from "./components/Search/Search";
+import Auth from "./components/Auth/Auth";
+import Bookmarks from "./components/Bookmarks/Bookmarks";
+import UserSettings from "./components/UserSettings/UserSettings";
+import ProtectedRoute from "./routes/ProtectedRoute/ProtectedRoute";
+
+import App from "./App";
+import "./index.css";
 
 // Supertokens init
 SuperTokens.init({
   appInfo: {
-    appName: "newsscraperauth",
-    apiDomain: "http://localhost:8080",
-    websiteDomain: "http://localhost:3000",
+    appName: import.meta.env.VITE_ST_APPNAME,
+    apiDomain: import.meta.env.VITE_ST_APIDOMAIN,
+    websiteDomain: import.meta.env.VITE_ST_WEBSITEDOMAIN,
     apiBasePathL: "/auth",
     websiteBasePath: "/auth",
   },
-  recipeList: [EmailPassword.init(), Session.init()],
+  recipeList: [
+    EmailPassword.init({
+      signInAndUpFeature: {
+        disableDefaultImplementation: true,
+      },
+      onHandleEvent: async (context) => {
+        if (context.action === "SUCCESS") {
+          if (context.user) {
+            location.reload();
+          }
+        }
+      },
+    }),
+    Session.init(),
+  ],
 });
 
 Session.addAxiosInterceptors(axiosI);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <UserProvider>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
           <Route index element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route
+            path="/user/bookmarks"
+            element={
+              <ProtectedRoute>
+                <Bookmarks />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/user/settings"
+            element={
+              <ProtectedRoute>
+                <UserSettings />
+              </ProtectedRoute>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+  </UserProvider>
 );
