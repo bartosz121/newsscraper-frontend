894954bbd78351ffdf83cb315001d7c99aa67fea
bartosz121 <bmagiera121@gmail.com>
2022-05-31 19:16:58 +0200

    Moved all logic to InfiniteScrollData component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,44 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import axios from "axios";
-import InfiniteScroll from "react-infinite-scroll-component";
 
 import Article from "../Article/Article";
-import Spinner from "../Spinner/Spinner";
+import InfiniteScrollData from "../InfiniteScrollData/InfiniteScrollData";
 
 const Home = () => {
-  const [news, setNews] = useState([]);
-  const [newsPage, setNewsPage] = useState(1);
-  const [hasMore, setHasMore] = useState(true);
-
-  const getFetchUrl = () => {
-    return `https://newsscraperapi.bartoszmagiera.me/api/v1/news?page=${newsPage}`;
-  };
-
-  const fetchNews = async () => {
-    const response = await axios.get(getFetchUrl());
-    setNews((state) => [...state, ...JSON.parse(response.data.result)]);
-    setNewsPage((state) => state + 1);
-    setHasMore(response.data.hasNext);
-  };
-
-  useEffect(() => {
-    fetchNews();
-  }, []);
+  const url = import.meta.env.VITE_NEWSSCRAPER_API_URL;
 
   return (
-    <div className="w-full flex justify-center">
+    <div className="w-full flex justify-center bg-base-300">
       <div className="mt-4 w-full md:w-4/5 flex flex-row justify-center items-center overflow-auto">
-        <InfiniteScroll
-          className="flex flex-col md:flex-row md:flex-wrap justify-center items-center gap-4"
-          dataLength={news.length}
-          next={fetchNews}
-          hasMore={hasMore}
-          loader={<Spinner />}
-        >
-          {news.map((article, index) => (
-            <Article key={index} data={article} />
-          ))}
-        </InfiniteScroll>
+        <InfiniteScrollData
+          url={url}
+          MapComponent={Article}
+          axiosInstance={axios}
+        />
       </div>
     </div>
   );
